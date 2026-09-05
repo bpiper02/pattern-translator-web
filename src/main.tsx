5c956388f5689811dc7294d93a24d934ad84f388
bpiper02 <93992231+bpiper02@users.noreply.github.com>
2026-09-04 22:56:51 -0400

    feat: mount stem editor in the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { StemEditor } from "./components/StemEditor";
 import "./styles.css";
+import "./stemEditor.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <main className="machineShell stemEditorShell">
+        <StemEditor />
+      </main>
+    </>
   </React.StrictMode>
 );
