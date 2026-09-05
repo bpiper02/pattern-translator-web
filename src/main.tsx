afc821b08bc904088ebafebc27dbdb174dee6f45
bpiper02 <93992231+bpiper02@users.noreply.github.com>
2026-09-05 08:42:14 -0400

    style(split): load splitter styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./App";
 import "./styles.css";
 import "./stemEditor.css";
 import "./resample.css";
+import "./split.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
