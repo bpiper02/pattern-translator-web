915e96fd68d707899e0ee1628f48c4d985403dcb
bpiper02 <93992231+bpiper02@users.noreply.github.com>
2026-09-05 01:25:43 -0400

    fix(ui): keep editor scoped to workspace and load resample styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { StemEditor } from "./components/StemEditor";
 import "./styles.css";
 import "./stemEditor.css";
+import "./resample.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <>
-      <App />
-      <main className="machineShell stemEditorShell">
-        <StemEditor />
-      </main>
-    </>
+    <App />
   </React.StrictMode>
 );
